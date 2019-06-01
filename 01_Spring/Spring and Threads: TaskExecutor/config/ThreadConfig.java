d6a5df2ad32ce693b5dbd6c052938cfcb25352fa
Shahid1993 <31109285+Shahid1993@users.noreply.github.com>
2019-06-01 13:11:42 +0530

    Create ThreadConfig.java

---
+++ 01_Spring/Spring and Threads: TaskExecutor/config/ThreadConfig.java
@@ -0,0 +1,28 @@
+package com.gkatzioura.config;
+ 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+ 
+import java.util.concurrent.Executor;
+ 
+/**
+ * Created by Shahid on 01/06/19.
+ */
+@Configuration
+public class ThreadConfig {
+ 
+    @Bean
+    public TaskExecutor threadPoolTaskExecutor() {
+ 
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(4);
+        executor.setThreadNamePrefix("default_task_executor_thread");
+        executor.initialize();
+ 
+        return executor;
+    }
+ 
+}
